fbe4b75126374545cc10d5024e9c2a7c38179868
LeeSuIl <suil2010@naver.com>
2017-11-24 19:32:58 +0900

    mapper 수정

--- Plzdog/src/main/java/com/plzdog/vo/Code.java
+++ Plzdog/src/main/java/com/plzdog/vo/Code.java
@@ -4,14 +4,27 @@ import java.io.Serializable;
 
 public class Code implements Serializable{
 
+	private String code;
 	private String codeName;
 	private String category;
 
 	public Code() {}
 
-	public Code(String codeName, String category) {
+	public Code(String code, String codeName, String category) {
+		super();
+		this.code = code;
 		this.codeName = codeName;
 		this.category = category;
+	}
+	
+	
+	
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
 	}
 
 	public String getCodeName() {
@@ -31,10 +44,16 @@ public class Code implements Serializable{
 	}
 
 	@Override
+	public String toString() {
+		return "Code [code=" + code + ", codeName=" + codeName + ", category=" + category + "]";
+	}
+
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((category == null) ? 0 : category.hashCode());
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
 		result = prime * result + ((codeName == null) ? 0 : codeName.hashCode());
 		return result;
 	}
@@ -53,16 +72,16 @@ public class Code implements Serializable{
 				return false;
 		} else if (!category.equals(other.category))
 			return false;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
 		if (codeName == null) {
 			if (other.codeName != null)
 				return false;
 		} else if (!codeName.equals(other.codeName))
 			return false;
 		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Code [codeName=" + codeName + ", category=" + category + "]";
 	}
 }
