a9805c30d4a55e162d15521ea6acb8f0c2dc42eb
LeeSuIl <suil2010@naver.com>
2017-11-25 17:36:19 +0900

    reservition 수정

--- Plzdog/src/main/java/test/test.java
+++ Plzdog/src/main/java/test/test.java
@@ -1,7 +1,16 @@
 package test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 public class test {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 	}
 }
