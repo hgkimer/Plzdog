4cf23baa607505f411af92f98e0576304afe8bb0
LeeHyoJu <mint0490@naver.com>
2017-11-25 14:17:28 +0900

    스프링끝

--- Plzdog/src/main/java/com/plz/dao/impl/AuthorityDaoImpl.java
+++ Plzdog/src/main/java/com/plz/dao/impl/AuthorityDaoImpl.java
@@ -1,5 +1,10 @@
 package com.plz.dao.impl;
 
-public class AuthorityDaoImpl {
+import org.springframework.stereotype.Repository;
+
+import com.plz.dao.AuthorityDao;
+
+@Repository
+public class AuthorityDaoImpl implements AuthorityDao {
 
 }
