81f2b5b2ffae979737e017df60ab1f97568aedb3
kimhogyu <hgkimer@naver.com>
2017-11-25 14:01:31 +0900

    AuthorityDao랑 DaoImpl함, 아직 테스트는 못함

--- Plzdog/src/main/java/com/plz/dao/AuthorityDao.java
+++ Plzdog/src/main/java/com/plz/dao/AuthorityDao.java
@@ -1,5 +1,43 @@
 package com.plz.dao;
 
-public interface AuthorityDao {
+import java.util.List;
 
+import com.plzdog.vo.Authority;
+
+public interface AuthorityDao {
+	/**
+	 * 권한을 추가하는 메소드
+	 * @param authority
+	 * @return
+	 */
+	int insertAuthority(Authority authority);
+	
+	/**
+	 * 권한을 수정하는 메소드
+	 * @param authority
+	 * @return
+	 */
+	int updateAuthority(Authority authority);
+	
+	/**
+	 * 매개변수로 받은 email에 해당하는 권한을 삭제하는 메소드.
+	 * @param email
+	 * @return
+	 */
+	int deleteAuthority(String email);
+	
+	/**
+	 * 전체 email과 권한을 조회하는 메소드
+	 * @return
+	 */
+	List<Authority> selectAllAuthority();
+	
+	/**
+	 * 매개변수로 받은 email로 권한을 조회하는 메소드
+	 * @param email
+	 * @return
+	 */
+	Authority selectAuthorityByEmail(String email);
+	
+	
 }
