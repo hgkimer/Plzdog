784ce9ddc9d0c25f732747b2c5aa5d3be7d167d3
Kimhogyu <hgkimer@naver.com>
2017-11-27 06:14:29 +0900

    Member dao, daoimpl 구현 완료. 이따 코스타가서 ServiceImpl + test

--- Plzdog/src/main/java/com/plz/dao/MemberDao.java
+++ Plzdog/src/main/java/com/plz/dao/MemberDao.java
@@ -1,5 +1,71 @@
 package com.plz.dao;
 
-public interface MemberDao {
+import java.util.List;
 
+import com.plzdog.vo.Member;
+
+public interface MemberDao {
+	/**
+	 * 매개변수로 받은 회원의 정보를 저장하는 메소드
+	 * @param member
+	 * @return
+	 */
+	int insertMember(Member member);
+	/**
+	 * 매개변수로 받은 email로 회원을 삭제하는 메소드
+	 * @param email
+	 * @return
+	 */
+	int deleteMember(String email);
+	/**
+	 * 매개변수로 받은 회원의 정보를 수정하는 메소드
+	 * @param member
+	 * @return
+	 */
+	int updateMemeber(Member member);
+	/**
+	 * 전체 회원을 조회하는 메소드
+	 * @return
+	 */
+	List<Member> selectAllMember();
+	/**
+	 * 매개변수로 받은 email로 회원을 조회하는 메소드
+	 * @param email
+	 * @return
+	 */
+	Member selectMemberByEmail(String email);
+	/**
+	 * 매개변수로 받은 회원이름과 일치하는 회원을 조회하는 메소드(부분일치)
+	 * @param memberName
+	 * @return
+	 */
+	List<Member> selectMemberByName(String memberName);
+	/**
+	 * 매개변수로 받은 펫시터 정보를 조회하는 메소드
+	 * 	+ sitter 조인
+	 * @return
+	 */
+	List<Member> selectAllMemberJoinSitter();
+	/**
+	 * 이메일로 펫시터 정보를 조회하는 메소드
+	 * 	+ sitter 조인
+	 * @param email
+	 * @return
+	 */
+	Member selectMemberJoinSitterByEmail(String email);
+	/**
+	 * 매개변수로 받은 이름으로 펫시터 정보를 조회하는 메소드
+	 * 	+ sitter 조인
+	 * @param name
+	 * @return
+	 */
+	List<Member> selectMemberJoinSitterByName(String name);
+	/**
+	 * 매개변수로 받은 이름으로 회원의 정보와 회원이 작성한 후기(Review)를 조회하는 메소드
+	 * 	+ review 조인
+	 * @param name
+	 * @return
+	 */
+	//TODO: 어디에 쓰는 SELECT문??
+	List<Member> selectMemberJoinReview(String name);
 }
--- Plzdog/src/main/java/com/plz/service/MemberService.java
+++ Plzdog/src/main/java/com/plz/service/MemberService.java
@@ -1,5 +1,65 @@
 package com.plz.service;
 
-public interface MemberService {
+import java.util.List;
 
+import com.plzdog.vo.Member;
+
+public interface MemberService{
+	/**
+	 * 한 명의 회원을 등록하는 메소드
+	 */
+	void addMember();
+	/**
+	 * 매개변수로 받은 회원의 정보를 삭제하는 메소드
+	 * @param email
+	 */
+	void deleteMember(Member member);
+	/**
+	 * 매개변수로 받은 회원의 정보를 수정하는 메소드
+	 * @param member
+	 */
+	void updateMember(Member member);
+	/**
+	 * 모든 회원의 정보를 조회하는 메소드
+	 * @return
+	 */
+	List<Member> findAllMember();
+	/**
+	 * 매개변수로 받은 email로 회원을 조회하는 메소드
+	 * @param email
+	 * @return
+	 */
+	Member findMemberByEmail(String email);
+	/**
+	 * 매개 변수로 받은 이름으로 회원을 조회하는 메소드
+	 * 	+ 부분일치
+	 * 	+ 이름 중복가능
+	 * @param name
+	 * @return
+	 */
+	List<Member> findMemberByName(String name);
+	/**
+	 * 기본 회원 정보와 시터 정보를 추가하여 전체 회원정보를 조회하느 메소드
+	 * @return
+	 */
+	List<Member> findMemberJoinSiiter();
+	/**
+	 * 매개 변수로 받은 이메일과 일치하는 기본 회원 정보 + 시터 정보를 조회하는 메소드
+	 * @param email
+	 * @return
+	 */
+	Member findMemberJoinSitterByEmail(String email);
+	/**
+	 * 매개 변수로 받은 이름과 일치하는 기본 회원 정보 + 시터 정보를 조회하는 메소드
+	 * @param name
+	 * @return
+	 */
+	List<Member> findMemberJoinSitterByName(String name);
+	/**
+	 * 매개 변수로 받은 이름과 일치하는 기본회원 정보와 리뷰정보를 조회하는 메소드........
+	 * TODO:음.... 어따 쓰지
+	 * @param name
+	 * @return
+	 */
+	List<Member> findMemberJoinReview(String name);
 }
