7cb38af4f75e9ff74b81cb7936c2e5871efc0f9d
LeeSuIl <suil2010@naver.com>
2017-11-27 14:29:21 +0900

    reservation 수정?

--- Plzdog/src/main/java/test/ReservationTest.java
+++ Plzdog/src/main/java/test/ReservationTest.java
@@ -1,11 +1,19 @@
 package test;
 
+import java.util.Date;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.plz.service.CareService;
+import com.plz.dao.ReservationDao;
+import com.plzdog.vo.Reservation;
 
 public class ReservationTest {
-	ApplicationContext ctx = new ClassPathXmlApplicationContext("com/plz/config/spring/web-context.xml");
-	CareService service = (CareService) ctx.getBean("careServiceImpl");
+	
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/plz/config/spring/web-context.xml");
+		ReservationDao dao = (ReservationDao) ctx.getBean("ReservationDaoImpl");
+	
+		dao.insertReservation(new Reservation(0,1,new Date("2017/11/27"),new Date("2017/11/28"),"저희 강아지가 행복했으면 좋겠어요.","kim@naver.com","lee@naver.com"));	
+	}
 }
