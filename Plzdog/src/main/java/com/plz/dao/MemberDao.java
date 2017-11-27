c7399c96d462735b58ce4218f40b56d0a193b664
kimhogyu <hgkimer@naver.com>
2017-11-27 09:23:59 +0900

    풀용 커밋

--- Plzdog/src/main/java/com/plz/dao/MemberDao.java
+++ Plzdog/src/main/java/com/plz/dao/MemberDao.java
@@ -1,5 +1,16 @@
 package com.plz.dao;
 
-public interface MemberDao {
+import java.util.List;
 
+import com.plzdog.vo.Member;
+
+public interface MemberDao {
+	/**
+	 * 매개변수로 받은 회원정보를 저장하는 메소드
+	 * @param member
+	 * @return
+	 */
+	int insertMember(Member member);
+	
+	
 }
