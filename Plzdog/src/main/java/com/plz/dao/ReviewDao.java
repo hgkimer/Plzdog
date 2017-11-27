8357f2fa61d5a18216aa433c77920fa243268321
LeeHyoJu <mint0490@naver.com>
2017-11-27 14:56:43 +0900

    화면 띄우기 성공

--- Plzdog/src/main/java/com/plz/dao/ReviewDao.java
+++ Plzdog/src/main/java/com/plz/dao/ReviewDao.java
@@ -1,5 +1,27 @@
 package com.plz.dao;
 
-public interface ReviewDao {
+import com.plzdog.vo.Review;
 
+public interface ReviewDao {
+	
+	/**
+	 * 리뷰 등록
+	 * @param reivew
+	 * @return
+	 */
+	int insertReview(Review review);
+
+	/**
+	 * 리뷰 수정
+	 * @param review
+	 * @return
+	 */
+	int updateReview(Review review);
+	
+	/**
+	 * 리뷰 삭제
+	 * @param reviewId
+	 * @return
+	 */
+	int deleteReview(int reviewId);
 }
