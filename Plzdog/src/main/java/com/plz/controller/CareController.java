a5a74b9d9633002179c5271ab9463d900af8983a
LeeHyoJu <mint0490@naver.com>
2017-11-28 09:50:04 +0900

    설정파일 수정

--- Plzdog/src/main/java/com/plz/controller/CareController.java
+++ Plzdog/src/main/java/com/plz/controller/CareController.java
@@ -8,8 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.plz.service.CareService;
 import com.plzdog.vo.Care;
@@ -20,8 +20,8 @@ public class CareController {
 	@Autowired
 	private CareService service;
 	
-	@RequestMapping("/care/select_care")
-	public String selectCareJoinCareImage(@ModelAttribute int careId, HttpServletRequest request, ModelMap model) throws Exception {
+	@RequestMapping("/care/select_care")	//매개변수를 VO로 받을 땐 @ModelAttribute
+	public String selectCareJoinCareImage(@RequestParam int careId, HttpServletRequest request, ModelMap model) throws Exception {
 		List<Care> list = service.selectCareJoinCareImage(careId);
 		for(Care c : list) {
 			System.out.println(c);
--- Plzdog/src/main/webapp/WEB-INF/view/content/test/dog_test.jsp
+++ Plzdog/src/main/webapp/WEB-INF/view/content/test/dog_test.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html;charset=utf-8"%>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +8,8 @@
 </head>
 <body>
 <form action="${initParam.rootPath }/care/select_care.do" method="post">
-	<input type="number" id="id" name="id">
+	<sec:csrfInput/> <!-- post, 로그인, 로그아웃 방식은 sec를 꼭 붙여준다. 토큰을 보내줘야한다. -->
+	<input type="number" id="careId" name="careId">
 	<button type="submit">전송</button>
 </form>
 
